88e1b6342270dcf8483b68b89a3145459f1d571f
CatDany <dany2001ru@gmail.com>
2014-07-09 07:50:56 +0700

    Finished PacketGuiChange

--- src/main/java/oceany/network/packet/PacketGuiChange.java
+++ src/main/java/oceany/network/packet/PacketGuiChange.java
@@ -1,6 +1,7 @@
 package oceany.network.packet;
 
 import io.netty.buffer.ByteBuf;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.TileEntity;
 import oceany.network.packet.PacketGuiChange.GuiChangeMessage;
 import oceany.tile.TileOceanyCore;
@@ -17,7 +18,13 @@ public class PacketGuiChange implements IMessageHandler<GuiChangeMessage, IMessa
 	{
 		if (ctx.side.isServer())
 		{
-			
+			if (message.tile instanceof TileOceanyInfuser)
+			{
+				if (message.element == 0)
+				{
+					((TileOceanyInfuser)message.tile).isEjecting = message.value == 1;
+				}
+			}
 		}
 		return null;
 	}
@@ -38,7 +45,17 @@ public class PacketGuiChange implements IMessageHandler<GuiChangeMessage, IMessa
 		@Override
 		public void fromBytes(ByteBuf buf)
 		{
-			
+			byte id = buf.readByte(); 
+			int x = buf.readInt();
+			int y = buf.readInt();
+			int z = buf.readInt();
+			int dim = buf.readInt();
+			if (tileIds[id].isInstance(MinecraftServer.getServer().worldServers[dim].getTileEntity(x, y, z)))
+			{
+				this.tile = (TileEntity)tileIds[id].cast(MinecraftServer.getServer().worldServers[dim].getTileEntity(x, y, z));
+			}
+			this.element = buf.readByte();
+			this.value = buf.readByte();
 		}
 		
 		@Override
@@ -48,6 +65,7 @@ public class PacketGuiChange implements IMessageHandler<GuiChangeMessage, IMessa
 			buf.writeInt(tile.xCoord);
 			buf.writeInt(tile.yCoord);
 			buf.writeInt(tile.zCoord);
+			buf.writeInt(tile.getWorldObj().provider.dimensionId);
 			buf.writeByte(element);
 			buf.writeByte(value);
 		}
